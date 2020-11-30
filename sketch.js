b8d4dfe72c62af76c258cd4e9a5eb4f15ddb848c
JASH-1234 <74188388+JASH-1234@users.noreply.github.com>
2020-11-30 11:57:13 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,25 +1,33 @@
-var player,player2;
+var car,wall,speed,weight;
 function setup() {
-  createCanvas(800,400);
- player = createSprite(400, 200, 50, 50);
- player2 = createSprite(200,300,50,50);
+  createCanvas(1600,400);
+  car = createSprite(100,200,50,50);
+  car.shapeColor="black"
+  car.velocityX = speed;
+  car.velocityX = 30;
+  
+  wall=createSprite(1400,200,60,height/2)
+  wall.shapeColor=(80,80,80);
+
 }
 
 function draw() {
-  background("blue");  
+  background("ORANGE");  
   drawSprites();
-  player.y=mouseY;
-  player.x=mouseX;
-  player.velocityX=2;
-  player.velocityY=2;
-  player2.velocityX=3;
-  player2.velocityY=3;
-  if(player.x-player2.x<=player.width/2+player2.width/2&& player2.x-player.x<=player.width/2+player2.width/2){
-    player.velocityX*=-1;
-    player2.velocityX*=-1;
-  }
-  if(player.y-player2.y<=player.height/2+player2.height/2&& player2.y-player.y<=player.height/2+player2.height/2){
-     player.velocityY*=-1;
-     player2.velocityY=player2.velocityY*-1;
+  speed=random(55,90);
+  weight=random(400,1500)
+  if(wall.x-car.x<(car.width/2+wall.width/2)){
+    car.velocityX=0;
+    var deformation=0.5 *weight *speed *speed /22509;
+    if(deformation>180){
+      car.shapeColor=color(225,0,0);
+    }
+    if(deformation<180 && deformation>100){
+      car.shapeColor=color(230,230,0);
+    } 
+    if(deformation<100){
+      car.shapeColor=color(0,225,0);
+    }
+    
   }
 }
